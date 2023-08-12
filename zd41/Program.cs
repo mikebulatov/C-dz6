d19b305af7dac0fceb2975654c73cfad04ce1270
Mikebulatov <mikebulatov@yandex.ru>
2023-08-12 22:52:31 +0700

    Задача 41.

---
+++ zd41/Program.cs
@@ -0,0 +1,25 @@
+﻿bool Even(int num)
+{// фнукция определяет четное число или нет.
+if (num%2>0)return false;
+else return true;
+}
+
+Console.WriteLine("Задача 34. Программа, которая покажет количество чётных чисел в массиве,заполненномы случайными положительными трёхзначными числами.");
+Console.Write("Введите колличесво элементов маассива:");
+int [] mass=new int [Convert.ToInt32(Console.ReadLine())];
+
+Console.Write("Сформированный массив:");
+for(int i=0; i<mass.Length; i++)
+{
+    mass[i]= new Random().Next(100,999);
+    Console.Write(mass[i]);
+    Console.Write(" ");
+}
+
+Console.WriteLine();
+int CountEven =0;
+for(int i=0; i<mass.Length; i++)
+{
+if (Even(mass[i]))CountEven++;
+}
+Console.WriteLine($"Число четных элеменов массива:{CountEven}");
